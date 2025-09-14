fa6fe50454e68acdcbf683711d43394058d16d53
Dechiviel <farissoehartono@gmail.com>
2025-09-14 17:34:02 +0700

    Add migration and models for department, position, employee, salary, and attendance

---
+++ app-pegawai/app/Models/Attendance.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Attendance extends Model
+{
+    protected $table = "attendances";
+    protected $fillable = [
+        'karyawan_id',
+        'tanggal',
+        'waktu_masuk',
+        'waktu_keluar',
+        'status_absensi',
+    ];
+}
